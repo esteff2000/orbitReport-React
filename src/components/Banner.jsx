fe55d1c70780d36e67a9f69d77b28d3c566053ef
esteff2000 <steffensoner@gmail.com>
2024-03-20 20:03:51 -0500

    finished styling

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import styling from "./styling.css"
+
+export function Banner() {
+    return (
+        <div>
+        <header><h1>Orbit Report</h1></header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    );
+}
+
+
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,20 @@
+import satData from "./satData";
+import styling from "./styling.css"
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+  return (
+    <button onClick={() => filterByType(sat)} key={id}>
+      {sat} Orbit
+    </button>
+  );
+})}
+  <button onClick={() => setSat(satData)}>All orbits</button>
     </div>
   );
 };
+
 
 export default Buttons;
